6802b674e235f243e6e3c089ea71695d1131d284
akira888 <a.cup.of.happiness@gmail.com>
2023-09-18 00:10:50 +0900

    games migrateファイルの修正

--- db/migrate/20230917150346_create_games.rb
+++ db/migrate/20230917150346_create_games.rb
@@ -2,12 +2,12 @@ class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
       t.string :title
-      t.integer :max_winners
-      t.integer :max_players
-      t.integer :entry_period_minutes
+      t.integer :max_winners, limit: 2, null: false, default: 1
+      t.integer :max_players, limit: 3, null: false, default: 1
+      t.integer :entry_period_minutes, limit: 2, null: false, default: 5
       t.datetime :started_at
       t.datetime :finished_at
-      t.boolean :random_awards
+      t.boolean :random_awards, null: false, default: false
       t.json :emit_numbers
 
       t.timestamps
