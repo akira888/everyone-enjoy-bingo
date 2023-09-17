c9a77e84c4688eba62be3cb4aa82c44c0639ea0c
akira888 <a.cup.of.happiness@gmail.com>
2023-09-18 00:35:34 +0900

    winnersのmigration調整

--- db/migrate/20230917153413_create_winners.rb
+++ db/migrate/20230917153413_create_winners.rb
@@ -4,7 +4,7 @@ class CreateWinners < ActiveRecord::Migration[7.0]
       t.references :game, null: false, foreign_key: true
       t.references :award, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.text :nessage
+      t.text :message
 
       t.timestamps
     end
