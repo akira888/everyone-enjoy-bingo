30486e1855db30e83ca17bc0d276f43617ae297f
akira888 <a.cup.of.happiness@gmail.com>
2023-09-18 00:26:54 +0900

    ownersのmigration調整

--- db/migrate/20230917152459_create_owners.rb
+++ db/migrate/20230917152459_create_owners.rb
@@ -2,7 +2,7 @@ class CreateOwners < ActiveRecord::Migration[7.0]
   def change
     create_table :owners do |t|
       t.references :game, null: false, foreign_key: true
-      t.string :uniq_id
+      t.string :uniq_id, null: false
       t.string :hashed_password
 
       t.timestamps
