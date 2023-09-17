90be6daceb07a98cf712d752fd7be916ceb3907b
akira888 <a.cup.of.happiness@gmail.com>
2023-09-18 00:25:57 +0900

    bin/rails g model Owner game:references uniq_id:string hashed_password:string

---
+++ db/migrate/20230917152459_create_owners.rb
@@ -0,0 +1,11 @@
+class CreateOwners < ActiveRecord::Migration[7.0]
+  def change
+    create_table :owners do |t|
+      t.references :game, null: false, foreign_key: true
+      t.string :uniq_id
+      t.string :hashed_password
+
+      t.timestamps
+    end
+  end
+end
