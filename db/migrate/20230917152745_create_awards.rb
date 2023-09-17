499876f4f4ebc26cec3f3a3306fc02582e84545a
akira888 <a.cup.of.happiness@gmail.com>
2023-09-18 00:28:37 +0900

    awardsのmigration調整

--- db/migrate/20230917152745_create_awards.rb
+++ db/migrate/20230917152745_create_awards.rb
@@ -2,7 +2,7 @@ class CreateAwards < ActiveRecord::Migration[7.0]
   def change
     create_table :awards do |t|
       t.references :game, null: false, foreign_key: true
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
