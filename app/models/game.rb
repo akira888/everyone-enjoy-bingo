81cbe75c32442a26dd0599d90cfdc0cd896f17b7
akira888 <a.cup.of.happiness@gmail.com>
2023-09-21 23:22:55 +0900

    参加者用、主催者用URLを生成してゲームを保存する

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,17 @@
 class Game < ApplicationRecord
+  before_save :set_url_hashes
+
+
+  private
+
+  def set_url_hashes
+    self.players_url_hash = unique_url_hash(:players_url_hash)
+    self.owners_url_hash = unique_url_hash(:owners_url_hash)
+  end
+
+  def unique_url_hash(field)
+    hash = SecureRandom.urlsafe_base64(8)
+
+    Game.find_by(field => hash) ? unique_url_hash(field) : hash
+  end
 end
