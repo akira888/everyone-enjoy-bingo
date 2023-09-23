bcb00b32e373c37a04a7379ccd2a841f8211af4a
akira888 <a.cup.of.happiness@gmail.com>
2023-09-23 15:19:31 +0900

    routingはnestする、オーナーは削除されない

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :games do
-    resources :owners
+    resources :owners, except: %i[destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
