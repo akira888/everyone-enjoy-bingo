06cc10a49e770425bcdffcfd6a3e0d258d31b38b
akira888 <a.cup.of.happiness@gmail.com>
2023-09-23 15:18:40 +0900

    routingはnestする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :owners
-  resources :games
+  resources :games do
+    resources :owners
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
