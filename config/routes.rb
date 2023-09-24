aad3acfa184c10a57fac83a697ef27ac41b1344f
akira888 <a.cup.of.happiness@gmail.com>
2023-09-25 01:32:13 +0900

    aasm用アクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :games, param: :owners_url_hash do
     resource :owner, except: %i[edit, update, destroy]
+    member do
+      patch :waiting
+      patch :start
+      patch :finish
+    end
   end
 end
