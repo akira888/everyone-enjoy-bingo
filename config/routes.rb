b2f04d47499aef55bbab66de45d52f9119215372
akira888 <a.cup.of.happiness@gmail.com>
2023-09-25 01:31:40 +0900

    routes.rbの構成変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "games#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :games, param: :owners_url_hash do
     resource :owner, except: %i[edit, update, destroy]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "games#index"
 end
