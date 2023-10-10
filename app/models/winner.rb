9a8325ad64f8e5ae2ca07bf283bb9f7bbd4c084b
akira888 <a.cup.of.happiness@gmail.com>
2023-10-10 16:59:08 +0900

    winnersの必須カラム定義、賞品はあとから入ることもあるのでoptionalとする

--- app/models/winner.rb
+++ app/models/winner.rb
@@ -1,5 +1,8 @@
 class Winner < ApplicationRecord
   belongs_to :game
-  belongs_to :award
+  belongs_to :award, optional: true
   belongs_to :user
+
+  validates :game_id, presence: true
+  validates :user_id, presence: true
 end
