1bdac98da81a4a3be3ddde1fa0b67f7eaf9bdd88
akira888 <a.cup.of.happiness@gmail.com>
2023-10-10 11:15:07 +0900

    賞品に優先順位がないと渡し間違える

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_134505) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_021308) do
   create_table "awards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "game_id", null: false
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_number", default: 1, null: false
     t.index ["game_id"], name: "index_awards_on_game_id"
   end
 
