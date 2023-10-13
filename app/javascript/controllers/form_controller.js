7e47df25b7cf50ae024d44897776c92edaa32054
akira888 <a.cup.of.happiness@gmail.com>
2023-10-13 23:43:33 +0900

    numberの許可範囲をminからmaxに制御する

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,6 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form"
 export default class extends Controller {
-  connect() {
+
+  adjustInputNumber(event) {
+    if (event.target.type !== "number") return
+    const form = event.target
+
+    if (parseInt(form.value) < parseInt(form.min)) {
+      alert("Please enter a number greater than "+form.min)
+      form.value = form.min
+    }
+    if (parseInt(form.value) > parseInt(form.max)) {
+      alert("Please enter a number less than "+form.max)
+      form.value = form.max
+    }
   }
 }
