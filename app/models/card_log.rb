ddd109e7dda82d46d25ad6ed9d1fa99d206d192b
akira888 <a.cup.of.happiness@gmail.com>
2023-10-18 09:42:21 +0900

    主催者画面にゲーム状況がリアルタイムに伝わるようにした

--- app/models/card_log.rb
+++ app/models/card_log.rb
@@ -1,6 +1,8 @@
 class CardLog < ApplicationRecord
   belongs_to :card
 
+  after_create_commit -> { broadcast_update_to card.game.bingo_log_channel, target: "make-lines", partial: "games/make_lines", locals: { game: card.game } }
+
   scope :bingo_count, -> { where(action: :bingo).count }
   scope :one_left_count, -> { where(action: :one_left).count }
 
