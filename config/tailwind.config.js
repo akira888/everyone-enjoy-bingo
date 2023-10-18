00f2e27bfdcc8fa70e5ca3b5e32df3072d9bc1fa
akira888 <a.cup.of.happiness@gmail.com>
2023-10-18 23:39:07 +0900

    daisyuiのtheme導入

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
+  daisyui: {
+    themes: ["lemonade", "cupcake", "pastel"],
+  },
   theme: {
     extend: {
       fontFamily: {
